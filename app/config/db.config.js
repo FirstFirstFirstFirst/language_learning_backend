075409f56e9d5f37da5bd5b26f93614799879bbf
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2023-11-27 23:27:04 +0700

    change db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  // HOST: "localhost",
-  // USER: "root",
-  // PASSWORD: "",
-  // DB: "language_learning",
-
   HOST: "sql.freedb.tech",
-  USER: "freedb_free_db_user_api",
-  PASSWORD: "52zgtGTX%fapYNF",
   PORT: 3306,
-  DB: "freedb_myapi_db_test",
+  DB: "freedb_languageLearning",
+  USER: "freedb_learner",
+  PASSWORD: "@tXeqD3BGb#VJSe",
 };
